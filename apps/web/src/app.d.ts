380870764ef63d60521c8fa4ff7b4dcc471d099b
Joseph Nwudu <onwudu@gmail.com>
2025-12-26 03:25:02 -0800

    rebuild

--- apps/web/src/app.d.ts
+++
@@ -1,9 +0,0 @@
-/// <reference types="svelte" />
-/// <reference types="vite/client" />
-
-declare namespace App {
-  interface Locals {}
-  interface PageData {}
-  interface Error {}
-  interface Platform {}
-}
--- apps/web/src/lib/stores.ts
+++
@@ -1,9 +0,0 @@
-import { writable } from "svelte/store";
-
-export type UserRole = "consumer" | "business";
-
-export const userRole = writable<UserRole>("consumer");
-
-export const setUserRole = (role: UserRole) => {
-  userRole.set(role);
-};
